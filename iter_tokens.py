dde50887877fa5f42471b1d6e15600b67e29e148
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2026-06-04 14:34:10 +0300

    Add tokens PLUS, MINUS, ... WS,
    -            (val for val in self.__class__.__dict__.values() if isinstance(val, str))
    +            (val for key, val in self.__class__.__dict__.items() if key.isupper())

--- iter_tokens.py
+++ iter_tokens.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # PYTHON_ARGCOMPLETE_OK
-from typing import Any
+from typing import Any, MutableMapping
 from dataclasses import dataclass
+from collections import OrderedDict
 import re
 
 
@@ -16,21 +17,34 @@ class TokensMeta(type):
     def __new__(mcls, clsname, bases, clsdict):
         for key, val in clsdict.items():
             if key not in ("__module__", "__qualname__"):
+                # key: NAME, NUM, iter_tokens
                 if isinstance(val, str):
                     rval = rf"(?P<{key}>{val})"
                     clsdict[key] = rval
         return super().__new__(mcls, clsname, bases, clsdict)
 
+    @classmethod
+    def __prepare__(
+        metacls, name: str, bases: tuple[type, ...], /, **kwds: Any
+    ) -> MutableMapping[str, object]:
+        return OrderedDict()
+
 
 class Tokens(metaclass=TokensMeta):
     NAME = r"[A-Za-z](\w|\d)+"  # r"(?P<NAME>[A-Za-z](\w|\d)+)"
     NUM = r"\d+"  # r"(?P<NUM>\d+)"
+    PLUS = r"\+"
+    MINUS = r"-"
+    MUL = r"\*"
+    DIV = r"/"
+    LPAREN = r"\("
+    RPAREN = r"\)"
+    WS = r"\w+"
 
     def iter_tokens(self, str_to_parse):
         masterpat = "|".join(
-            (val for val in self.__class__.__dict__.values() if isinstance(val, str))
+            (val for key, val in self.__class__.__dict__.items() if key.isupper())
         )
-        print(f"{masterpat = }")
         for m in re.finditer(masterpat, str_to_parse):
             token = Token(m.lastgroup, m.group(0))
             yield token
