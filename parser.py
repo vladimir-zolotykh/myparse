9837e3a4a731bf4fc2e6016207d2b9c339e96146
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2026-06-05 21:33:34 +0300

    Decorate @log_tokens _advance, etc

--- parser.py
+++ parser.py
@@ -6,6 +6,7 @@ from typing import Iterator, Any
 from dataclasses import dataclass
 import logging
 from functools import wraps
+import copy
 from iter_tokens import Token, Tokens
 
 
@@ -62,8 +63,9 @@ def log_tokens(func):
     @wraps(func)
     def wrapper(*args, **kwds):
         self = args[0]
-        logging.info(f"[{func.__name__}] {self.cash!r}")
+        # cash0 = copy.copy(self.cash)
         res = func(*args, **kwds)
+        logging.info(f"[{func.__name__}] {self.cash!r}")
         return res
 
     return wrapper
@@ -73,6 +75,7 @@ class TokensCash:
     def __init__(self, str_to_parse: str):
         self._tokens = Tokens().iter_tokens(str_to_parse)
         self._cash: list[Token] = list(self._tokens)
+        logging.info(f"[TokensCash.__init__] {self._cash!r}")
         self._index = 0
 
     def next(self) -> Token:
@@ -101,6 +104,7 @@ class Parser:
         self._advance()
         return self.expr()
 
+    @log_tokens
     def _advance(self) -> Token | None:
         try:
             # self.tok = next(self.tokens)
@@ -109,11 +113,13 @@ class Parser:
         except StopIteration:
             return None
 
+    @log_tokens
     def _expect(self, expected: str):
         if self.tok.val != expected:
             raise SyntaxError(f"{self.tok.val}: Expected {expected}")
         self._consume()
 
+    @log_tokens
     def _consume(self) -> None:
         # self.tok = next(self.tokens)
         self.tok = self.cash.next()
@@ -123,36 +129,37 @@ class Parser:
         res = self.term()
         while (op := self._advance()) and op.val in ("+", "-"):
             right = self.term()
-            res = Plus("+", res, right) if op == "+" else Minus("-", res, right)
+            res = Plus("+", res, right) if op.val == "+" else Minus("-", res, right)
         return res
 
     @log_tokens
     def term(self) -> Node:
         res = self.factor()
-        while (op := self._advance()) and op in ("*", "/"):
+        while (op := self._advance()) and op.val in ("*", "/"):
             right = self.factor()
-            res = Mul("*", res, right) if op == "*" else Div("/", res, right)
+            res = Mul("*", res, right) if op.val == "*" else Div("/", res, right)
         return res
 
     @log_tokens
     def factor(self) -> Node:
-        tok: Token = self._advance()
-        # tok = self.tok
-        if tok == "(":
+        # tok: Token = self._advance()
+        tok = self.tok
+        if tok.val == "(":
             self._consume()
             res = self.expr()
             self._expect("(")
         else:
             res = Num(tok.val)
             self._advance()
-            return res
+        return res
 
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
     p = Parser()
-    res: Node = p.parse("2")
+    # res: Node = p.parse("2")
     res = p.parse("2 + 3")
-    res = p.parse("2 + 3 * 4")
-    res = p.parse("2 + (3 + 4) * 5")
-    res = p.parse("2 + (3 + * 4)")
+    print(res)
+    # res = p.parse("2 + 3 * 4")
+    # res = p.parse("2 + (3 + 4) * 5")
+    # res = p.parse("2 + (3 + * 4)")
