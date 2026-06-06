292db2e30474f137682e915dc0681cde06397ff1
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2026-06-06 10:18:36 +0300

    Add basic tests
    Add test_valid_expressions, test_invalid_expression

---
+++ tests/test_expressions.py
@@ -0,0 +1,56 @@
+import pytest
+from parser import Parser, Num, Plus, Mul
+
+
+@pytest.mark.parametrize(
+    ("expr", "expected"),
+    [
+        (
+            "2",
+            Num(2.0),
+        ),
+        (
+            "2 + 3",
+            Plus(
+                "+",
+                Num(2.0),
+                Num(3.0),
+            ),
+        ),
+        (
+            "2 + 3 * 4",
+            Plus(
+                "+",
+                Num(2.0),
+                Mul(
+                    "*",
+                    Num(3.0),
+                    Num(4.0),
+                ),
+            ),
+        ),
+        (
+            "2 + (3 + 4) * 5",
+            Plus(
+                "+",
+                Num(2.0),
+                Mul(
+                    "*",
+                    Plus(
+                        "+",
+                        Num(3.0),
+                        Num(4.0),
+                    ),
+                    Num(5.0),
+                ),
+            ),
+        ),
+    ],
+)
+def test_valid_expressions(expr, expected):
+    assert Parser().parse(expr) == expected
+
+
+def test_invalid_expression():
+    with pytest.raises(SyntaxError, match=r"\*.*Expected \( or NUMBER"):
+        Parser().parse("2 + (3 + * 4)")
